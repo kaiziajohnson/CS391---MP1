e125245ff14a51501aea4e5d4a81e18e896804cb
kaiziajohnson <kaiziajohnson@gmail.com>
2025-09-16 23:51:39 -0400

    updated website

--- index.js
+++ index.js
@@ -1,7 +1,73 @@
-// let one = document.getElementById("one");
-// let two = document.getElementById("two");
-// let output = document.getElementById("output");
-//
-// function doAdd(){
-//     output.innerHTML = Number(one.value) + Number(two.value);
-// }
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let output = document.getElementById("output");
+
+function doAdd() {
+    output.innerHTML = String(Number(one.value) + Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkmagenta";
+    }
+}
+function doSubtract() {
+    output.innerHTML = String(Number(one.value) - Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkmagenta";
+    }
+}
+function doMultiply() {
+    output.innerHTML = String(Number(one.value) * Number(two.value)); if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkmagenta";
+    }
+}
+function doMultiply() {
+    output.innerHTML = String(Number(one.value) * Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkmagenta";
+    }
+}
+function doDivide() {
+    output.innerHTML = String(Number(one.value) / Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }    else{
+        output.style.color= "darkmagenta";
+    }
+}
+function doPower() {
+    let result= 1;
+    for (let i=0; i<Number(two.value); i++){
+        result*=Number(one.value);
+    }
+    output.innerHTML= String(result);
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkmagenta";
+    }
+}
+
+function doClear(){
+    one.value="";
+    two.value="";
+    output.innerHTML="";
+}
+
